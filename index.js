ee018d91cb9542fa8a1d9895ac89574ea95fca90
Fozle Rabbi <fozlerabbi9790@gmail.com>
2022-09-15 23:21:04 +0600

    booking code is running

--- index.js
+++ index.js
@@ -13,21 +13,36 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z6pgmwg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const serviceCollection = client.db("doctors_portal").collection("services");
+        const bookingCollection = client.db("doctors_portal").collection("booking");
 
-        app.get("/service", async(req, res)=>{
+        app.get("/service", async (req, res) => {
             const query = {};
             const cursore = serviceCollection.find(query);
             const services = await cursore.toArray();
             res.send(services);
         })
 
+        app.post("/booking", async (req, res) => {
+            const bookingInfo = req.body;
+            // console.log(bookingInfo.tritmentId);
+            // const query = { tritmentId: bookingInfo.tritmentId }
+            const query = { serviceName: bookingInfo.serviceName, fixedDate: bookingInfo.fixedDate, patientName: bookingInfo.patientName }
+            const exists = await bookingCollection.findOne(query);
+            // console.log(exists);
+            if (exists) {
+                return res.send({ success: false, bookingInfo: exists })
+            }
+            const result = await bookingCollection.insertOne(bookingInfo);
+            res.send({ success: true, result });
+        })
+
 
     }
-    finally{
+    finally {
 
     }
 
